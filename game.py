ad58e4d83d06d520e6584afb16e593f3c5f6e351
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-08-16 13:21:31 -0600

    added methods to game.py

--- game.py
+++ game.py
@@ -1,3 +1,18 @@
 class Game:
     def __init__(self):
         pass
+
+    def welcome_player(self):
+        pass
+
+    def instructions(self):
+        pass
+
+    def run_game(self):
+        pass
+
+    def game_round(self):
+        pass
+
+    def display_winner(self):
+        pass
--- player.py
+++ player.py
@@ -1,3 +1,4 @@
 class Player:
     def __init__(self, first_player, second_player):
-        pass
+        self.first_player = first_player
+        self.second_player = second_player
